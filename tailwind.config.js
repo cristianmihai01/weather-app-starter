7a20764e56628e5b7c52a247646798806eab48be
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-08-03 09:37:25 +0300

    added

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      primary: 'Poppins',
+    },
+    extend: {
+      backgroundImage: {
+        gradientBg: "url('/src/assets/img/bg.png')",
+      },
+      keyframes: {
+        shake: {
+          '0%': {
+            transform: 'translate(3px, 0)',
+          },
+          '50%': {
+            transform: 'translate(-3px, 0)',
+          },
+          '100%': {
+            transform: 'translate(0, 0)',
+          },
+        },
+      },
+      animation: {
+        shake: 'shake 150ms 2 linear',
+      },
+    },
+  },
+  plugins: [],
+};
